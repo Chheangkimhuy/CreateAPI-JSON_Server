97f22b311024b2c937e39a7df34a920a6f0a6017
chheangKimhuy <chhkimhuy56@gmail.com>
2025-01-22 17:48:14 +0700

    cors-commit

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
 const middleware = jsonServer.defaults();
+const cors = require('cors');
 
 
 server.use(middleware);
 server.use(router);
+server.use(cors());
 const PORT = 3000;
 
 server.listen(PORT, () =>{
