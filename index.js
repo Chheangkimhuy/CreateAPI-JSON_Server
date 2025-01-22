5660b90784fe7abbf52d43a56c8fe002cc9a4ac4
chheangKimhuy <chhkimhuy56@gmail.com>
2025-01-22 17:50:35 +0700

    secondcors-commit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
 const middleware = jsonServer.defaults();
-const cors = require('cors');
+const {cors} = require('cors'); 
 
 
 server.use(middleware);
 server.use(router);
-server.use(cors());
+server.use(cors()); //npm i cors
 const PORT = 3000;
 
 server.listen(PORT, () =>{
